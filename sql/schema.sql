fedfe2e6892dfb72fc44443eeb6b63ec3a573408
Petr Eremeykin <eremeykin@gmail.com>
2019-01-09 21:00:17 +0300

    Rearrange sql in schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -10,11 +10,86 @@
 -- seq for all sequences
 
 -- my own conventions:
--- colname_id => PRIMARY KEY 
+-- colname_id => PRIMARY KEY
 -- colname_fk => FOREIGN KEY
 
+
+
+DROP TABLE IF EXISTS job_types;
+DROP SEQUENCE IF EXISTS job_types_type_id_seq;
+DROP TABLE IF EXISTS locations;
+DROP SEQUENCE IF EXISTS locations_location_id_seq;
+DROP TABLE IF EXISTS vacancies;
+DROP SEQUENCE IF EXISTS vacancies_vacancy_id_seq;
+DROP TABLE IF EXISTS resumes;
+DROP SEQUENCE IF EXISTS resumes_resume_id_seq;
+DROP TABLE IF EXISTS employers;
+DROP SEQUENCE IF EXISTS employers_employer_id_seq;
+DROP TABLE IF EXISTS companies;
+DROP SEQUENCE IF EXISTS companies_company_id_seq;
+DROP TABLE IF EXISTS applicants;
+DROP SEQUENCE IF EXISTS applicants_applicant_id_seq;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS users_user_id_seq;
+
+
+
+CREATE SEQUENCE job_types_type_id_seq;
+CREATE TABLE job_types(
+      type_id integer NOT NULL DEFAULT nextval('job_types_type_id_seq'),
+      name character varying(500)[] NOT NULL,
+      CONSTRAINT job_types_type_id_pkey PRIMARY KEY (type_id)
+);
+
+COMMENT ON TABLE job_types
+    IS 'Таблица типов работы (полная,частичная занятость)';
+
+
+
+CREATE SEQUENCE locations_location_id_seq;
+CREATE TABLE locations(
+    location_id integer NOT NULL DEFAULT nextval('locations_location_id_seq'),
+    country  character varying(500)[] NOT NULL,
+    region   character varying(500)[] NOT NULL,
+    city     character varying(500)[] NOT NULL,
+    district character varying(500)[],
+    CONSTRAINT locations_location_id_pkey PRIMARY KEY (location_id)
+);
+
+COMMENT ON TABLE locations
+    IS 'Таблица локаций, вкакнсий и резюме.';
+
+
+
+CREATE SEQUENCE companies_company_id_seq;
+CREATE TABLE companies
+(
+    company_id integer NOT NULL DEFAULT nextval('companies_company_id_seq'),
+    CONSTRAINT companies_company_id_pkey PRIMARY KEY (company_id)
+);
+
+COMMENT ON TABLE companies
+    IS 'Таблица компаний, от лица которых работодатель размещает вакансию.';
+
+
+
+CREATE SEQUENCE vacancies_vacancy_id_seq;
+CREATE TABLE vacancies
+(
+    vacancy_id integer NOT NULL DEFAULT nextval('vacancies_vacancy_id_seq'),
+    company_fk integer NOT NULL,
+    CONSTRAINT vacancies_vacancy_id_pkey PRIMARY KEY (vacancy_id),
+    CONSTRAINT vacancies_company_fk_fkey FOREIGN KEY (company_fk)
+        REFERENCES companies (company_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+COMMENT ON TABLE vacancies
+    IS 'Таблица вакансий, размещенных компаниями.';
+
+
+
 CREATE SEQUENCE users_user_id_seq;
 CREATE TABLE users
 (
@@ -28,8 +103,7 @@ COMMENT ON TABLE users
     IS 'Таблица пользователей, которые могут залогиниться на сайт.';
 
 
-DROP TABLE IF EXISTS applicants;
-DROP SEQUENCE IF EXISTS applicants_applicant_id_seq;
+
 CREATE SEQUENCE applicants_applicant_id_seq;
 CREATE TABLE applicants
 (
@@ -46,21 +120,24 @@ COMMENT ON TABLE applicants
     IS 'Таблица соискателей, которые могут создавать резюме.';
 
 
-DROP TABLE IF EXISTS companies;
-DROP SEQUENCE IF EXISTS companies_company_id_seq;
-CREATE SEQUENCE companies_company_id_seq;
-CREATE TABLE companies
+
+CREATE SEQUENCE resumes_resume_id_seq;
+CREATE TABLE resumes
 (
-    company_id integer NOT NULL DEFAULT nextval('companies_company_id_seq'),
-    CONSTRAINT companies_company_id_pkey PRIMARY KEY (company_id)
+    resume_id integer NOT NULL DEFAULT nextval('resumes_resume_id_seq'),
+    applicant_fk integer UNIQUE NOT NULL,
+    CONSTRAINT resumes_resume_id_pkey PRIMARY KEY (resume_id),
+    CONSTRAINT resumes_resume_fk_fkey FOREIGN KEY (applicant_fk)
+        REFERENCES applicants (applicant_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
 
-COMMENT ON TABLE companies
-    IS 'Таблица компаний, от лица которых работодатель размещает вакансию.';
+COMMENT ON TABLE resumes
+    IS 'Таблица резюме, которые создаются соискателями.';
 
 
-DROP TABLE IF EXISTS employers;
-DROP SEQUENCE IF EXISTS employers_employer_id_seq;
+
 CREATE SEQUENCE employers_employer_id_seq;
 CREATE TABLE employers
 (
@@ -78,66 +155,5 @@ CREATE TABLE employers
         ON DELETE NO ACTION
 );
 
-COMMENT ON TABLE companies
+COMMENT ON TABLE employers
     IS 'Таблица работодателей, которые могут размещать вакансии.';
-
-DROP TABLE IF EXISTS resumes;
-DROP SEQUENCE IF EXISTS resumes_resume_id_seq;
-CREATE SEQUENCE resumes_resume_id_seq;
-CREATE TABLE resumes
-(
-    resume_id integer NOT NULL DEFAULT nextval('resumes_resume_id_seq'),
-    applicant_fk integer UNIQUE NOT NULL,
-    CONSTRAINT resumes_resume_id_pkey PRIMARY KEY (resume_id),
-    CONSTRAINT resumes_resume_fk_fkey FOREIGN KEY (applicant_fk)
-        REFERENCES applicants (applicant_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-);
-
-COMMENT ON TABLE resumes
-    IS 'Таблица резюме, которые создаются соискателями.';
-
-DROP TABLE IF EXISTS vacancies;
-DROP SEQUENCE IF EXISTS vacancies_vacancy_id_seq;
-CREATE SEQUENCE vacancies_vacancy_id_seq;
-CREATE TABLE vacancies
-(
-    vacancy_id integer NOT NULL DEFAULT nextval('vacancies_vacancy_id_seq'),
-    company_fk integer NOT NULL,
-    CONSTRAINT vacancies_vacancy_id_pkey PRIMARY KEY (vacancy_id),
-    CONSTRAINT vacancies_company_fk_fkey FOREIGN KEY (company_fk)
-        REFERENCES companies (company_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-);
-
-COMMENT ON TABLE vacancies
-    IS 'Таблица вакансий, размещенных компаниями.';
-
-DROP TABLE IF EXISTS locations;
-DROP SEQUENCE IF EXISTS locations_location_id_seq;
-CREATE SEQUENCE locations_location_id_seq;
-CREATE TABLE locations(
-    location_id integer NOT NULL DEFAULT nextval('locations_location_id_seq'),
-    country  character varying(500)[] NOT NULL,
-    region   character varying(500)[] NOT NULL,
-    city     character varying(500)[] NOT NULL,
-    district character varying(500)[],
-    CONSTRAINT locations_location_id_pkey PRIMARY KEY (location_id)
-);
-
-COMMENT ON TABLE locations
-    IS 'Таблица локаций, вкакнсий и резюме.';
-
-DROP TABLE IF EXISTS job_types;
-DROP SEQUENCE IF EXISTS job_types_type_id_seq;
-CREATE SEQUENCE job_types_type_id_seq;
-CREATE TABLE job_types(
-      type_id integer NOT NULL DEFAULT nextval('job_types_type_id_seq'),
-      name character varying(500)[] NOT NULL,
-      CONSTRAINT job_types_type_id_pkey PRIMARY KEY (type_id)
-);
-
-COMMENT ON TABLE job_types
-    IS 'Таблица типов работы (полная,частичная занятость)';
