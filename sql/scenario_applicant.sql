018df34b989aa107f9c1cb28195b5ff77484459d
Petr Eremeykin <eremeykin@gmail.com>
2019-01-13 11:48:00 +0300

    Add more applicant scenario items

--- sql/scenario_applicant.sql
+++ sql/scenario_applicant.sql
@@ -47,29 +47,74 @@ SELECT job_id, (
                 )
 FROM insert_job;
 
-SELECT  * FROM jobs;
+
 
 -- Посмотреть резюме
 SELECT first_name, family_name, contact_phone, contact_email, title, city, description, salary FROM jobs
     JOIN resumes ON (job_fk = job_id)
     JOIN applicants ON applicant_fk = applicant_id
     JOIN users ON user_fk = user_id
-    WHERE login='elon@musk.com'
+    WHERE login='elon@musk.com';
 
 
 
--- Редактировать резюме
+-- Редактировать резюме #3
 UPDATE jobs SET
-    salary = '[400000, 500000]'
-WHERE job_id = (SELECT job_fk FROM resumes WHERE resume_id = 1);
+    salary = '[430000, 520000]'
+WHERE job_id = (SELECT job_fk FROM resumes WHERE resume_id = 3) ;
 
 
 
 -- Удалить резюме
 WITH delete_resume AS (
-    DELETE FROM resumes WHERE resume_id = 1
+    DELETE FROM resumes WHERE resume_id = 3
     RETURNING job_fk
     )
 DELETE FROM jobs
 USING delete_resume
 WHERE jobs.job_id = delete_resume.job_fk;
+
+
+
+-- Заново создаем резюме, оно ещё понадобится
+WITH insert_job AS (
+        INSERT INTO jobs (title, city, description, salary)
+        VALUES ('Инженер', 'Москва','Большой опыт работы. Сооснователь компании PayPal; основатель, совладелец, генеральный директор и главный инженер компании SpaceX; генеральный директор и Chief Product Architect компании Tesla; был членом совета директоров компании SolarCity до её слияния с Tesla.',
+                '[400000, 500000]')
+        RETURNING job_id
+   )
+INSERT INTO resumes (job_fk, applicant_fk)
+SELECT job_id, (
+                SELECT applicant_id
+                    FROM applicants
+                    JOIN users ON (user_fk=user_id)
+                WHERE login='elon@musk.com'
+                )
+FROM insert_job;
+
+
+
+-- Откликнемся на вакансию инженера-конструктора в Москве
+INSERT INTO responses(vacancy_fk, appliсant_fk, message)
+VALUES (5, (SELECT applicant_id FROM applicants JOIN users ON user_fk = user_id WHERE login='elon@musk.com'),
+'Здравствуйте, меня заинтересовала вакансия инженера ЦПИР в Москве, я монго работал над созданием очень сложных систем управления для космической техники.');
+
+
+
+-- В это время нам предлагают работать технологом в том же ЦПИР
+INSERT INTO suggestions (resume_fk, employer_fk, vacancy_fk, message)
+VALUES (4, 3, 5,
+'Предлагаем вам пройти собеседование на должность технолга в Центр перспективных инженерных разработок.');
+
+
+
+-- Посмотреть список предложений для нас
+SELECT companies.name, euser.first_name, euser.family_name, euser.contact_phone, euser.contact_email, message FROM suggestions
+         JOIN resumes ON resume_fk = resume_id
+         JOIN applicants ON applicant_fk = applicant_id
+         JOIN users auser ON user_fk = user_id
+         JOIN vacancies ON vacancy_fk = vacancy_id
+         JOIN companies ON company_fk = company_id
+         JOIN employers ON employer_fk = employer_id
+         JOIN users euser on employers.user_fk = euser.user_id
+WHERE auser.login='elon@musk.com';
