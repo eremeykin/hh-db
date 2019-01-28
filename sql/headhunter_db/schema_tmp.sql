5130b1dcb5f48709fcc1b882600b0e61cf106015
Petr Eremeykin <eremeykin@gmail.com>
2019-01-28 19:26:22 +0300

    Save new_id during data generating

--- sql/headhunter_db/schema_tmp.sql
+++ sql/headhunter_db/schema_tmp.sql
@@ -15,13 +15,15 @@ CREATE TABLE tmp_job
     title VARCHAR(500),
     city VARCHAR(500),
     description VARCHAR(5000),
-    salary INT8RANGE
+    salary INT8RANGE,
+    new_id INTEGER NOT NULL DEFAULT NEXTVAL('job_job_id_seq')
 );
 
 CREATE TABLE tmp_company
 (
     company_id SERIAL,
-    name VARCHAR(500)
+    name VARCHAR(500),
+    new_id INTEGER NOT NULL DEFAULT NEXTVAL('company_company_id_seq')
 );
 
 CREATE TABLE tmp_vacancy
@@ -29,7 +31,8 @@ CREATE TABLE tmp_vacancy
     vacancy_id SERIAL,
     active BOOLEAN,
     company_id INTEGER,
-    job_id INTEGER
+    job_id INTEGER,
+    new_id INTEGER NOT NULL DEFAULT NEXTVAL('vacancy_vacancy_id_seq')
 );
 
 CREATE TABLE tmp_account
@@ -40,13 +43,15 @@ CREATE TABLE tmp_account
     first_name VARCHAR(500),
     family_name VARCHAR(500),
     contact_email VARCHAR(500),
-    contact_phone BIGINT
+    contact_phone BIGINT,
+    new_id INTEGER NOT NULL DEFAULT NEXTVAL('account_account_id_seq')
 );
 
 CREATE TABLE tmp_applicant
 (
     applicant_id SERIAL,
-    account_id INTEGER
+    account_id INTEGER,
+    new_id INTEGER NOT NULL DEFAULT NEXTVAL('applicant_applicant_id_seq')
 );
 
 CREATE TABLE tmp_resume
@@ -54,14 +59,16 @@ CREATE TABLE tmp_resume
     resume_id SERIAL,
     active BOOLEAN,
     applicant_id INTEGER,
-    job_id INTEGER
+    job_id INTEGER,
+    new_id INTEGER NOT NULL DEFAULT NEXTVAL('resume_resume_id_seq')
 );
 
 CREATE TABLE tmp_hr_manager
 (
     hr_manager_id SERIAL,
     account_id INTEGER,
-    company_id INTEGER
+    company_id INTEGER,
+    new_id INTEGER NOT NULL DEFAULT NEXTVAL('hr_manager_hr_manager_id_seq')
 );
 
 CREATE TABLE tmp_message
@@ -71,7 +78,8 @@ CREATE TABLE tmp_message
     account_id INTEGER,
     vacancy_id INTEGER,
     resume_id INTEGER,
-    text VARCHAR(5000)
+    text VARCHAR(5000),
+    new_id INTEGER NOT NULL DEFAULT NEXTVAL('message_message_id_seq')
 );
 
 CREATE TABLE tmp_read_message
@@ -79,6 +87,7 @@ CREATE TABLE tmp_read_message
   read_message_id SERIAL,
   message_id INTEGER,
   account_id INTEGER,
-  UNIQUE (message_id, account_id)
+  UNIQUE (message_id, account_id),
+  new_id INTEGER NOT NULL DEFAULT NEXTVAL('read_message_read_message_id_seq')
 );
 
--- sql/headhunter_db/transfer.sql
+++ sql/headhunter_db/transfer.sql
@@ -1,108 +1,62 @@
 -- account
-WITH ins AS (
-  INSERT INTO account (login, password, first_name, family_name, contact_email, contact_phone)
-  SELECT login, password, first_name, family_name, contact_email, contact_phone FROM tmp_account ORDER BY account_id ASC OFFSET 0 LIMIT 1000
-  RETURNING account_id
-)
-INSERT INTO map_account (new_id)
-SELECT account_id FROM ins;
-
+INSERT INTO account (account_id, login, password, first_name, family_name, contact_email, contact_phone)
+SELECT new_id, login, password, first_name, family_name, contact_email, contact_phone FROM tmp_account
+ORDER BY account_id ASC OFFSET 0 LIMIT 1000;
 
 -- job
-WITH ins AS (
-  INSERT INTO job (title,city, description, salary)
-  SELECT title,city, description, salary FROM tmp_job ORDER BY job_id ASC OFFSET 0 LIMIT 1000
-  RETURNING job_id
-)
-INSERT INTO map_job (new_id)
-SELECT job_id FROM ins;
-
+INSERT INTO job (job_id, title,city, description, salary)
+SELECT new_id, title,city, description, salary FROM tmp_job
+ORDER BY job_id ASC OFFSET 0 LIMIT 1000;
 
 -- company
-WITH ins AS (
-  INSERT INTO company (name)
-  SELECT name FROM tmp_company ORDER BY company_id ASC OFFSET 0 LIMIT 1000
-  RETURNING company_id
-)
-INSERT INTO map_company (new_id)
-SELECT company_id FROM ins;
+INSERT INTO company (company_id, name)
+SELECT new_id, name FROM tmp_company
+ORDER BY company_id ASC OFFSET 0 LIMIT 1000;
 
 -- hr_manager
-WITH ins AS (
-  INSERT INTO hr_manager (account_id, company_id)
-  SELECT map_account.new_id, map_company.new_id
-  FROM tmp_hr_manager
-  JOIN map_account ON (tmp_hr_manager.hr_manager_id=map_account.map_id)
-  JOIN map_company ON (tmp_hr_manager.company_id = map_company.map_id)
-  ORDER BY hr_manager_id ASC OFFSET 0 LIMIT 1000
-  RETURNING hr_manager_id
-)
-INSERT INTO map_hr_manager (new_id)
-SELECT hr_manager_id FROM ins;
+INSERT INTO hr_manager (hr_manager_id, account_id, company_id)
+SELECT tmp_hr_manager.new_id, tmp_account.new_id, tmp_company.new_id
+FROM tmp_hr_manager
+JOIN tmp_account USING (account_id)
+JOIN tmp_company USING (company_id)
+ORDER BY hr_manager_id ASC OFFSET 0 LIMIT 1000;
 
 -- applicant
-WITH ins AS (
-  INSERT INTO applicant (account_id)
-  SELECT map_account.new_id
-  FROM tmp_applicant
-  JOIN map_account ON (tmp_applicant.applicant_id = map_account.map_id)
-  ORDER BY applicant_id ASC OFFSET 0 LIMIT 1000
-  RETURNING applicant_id
-)
-INSERT INTO map_applicant (new_id)
-SELECT applicant_id FROM ins;
+INSERT INTO applicant (applicant_id, account_id)
+SELECT tmp_applicant.new_id, tmp_account.new_id FROM tmp_applicant
+JOIN tmp_account USING (account_id)
+ORDER BY applicant_id ASC OFFSET 0 LIMIT 1000;
 
 -- vacancy
-WITH ins AS (
-  INSERT INTO vacancy (active, company_id, job_id)
-  SELECT active, map_company.new_id, map_job.new_id
-  FROM tmp_vacancy
-  JOIN map_company ON (tmp_vacancy.company_id=map_company.map_id)
-  JOIN map_job ON (tmp_vacancy.job_id = map_job.map_id)
-  ORDER BY vacancy_id ASC OFFSET 0 LIMIT 1000
-  RETURNING vacancy_id
-)
-INSERT INTO map_vacancy (new_id)
-SELECT vacancy_id FROM ins;
+INSERT INTO vacancy (vacancy_id, active, company_id, job_id)
+SELECT tmp_vacancy.new_id, active, tmp_company.new_id, tmp_job.new_id
+FROM tmp_vacancy
+JOIN tmp_company USING (company_id)
+JOIN tmp_job USING (job_id)
+ORDER BY vacancy_id ASC OFFSET 0 LIMIT 1000;
 
 -- resume
-WITH ins AS (
-  INSERT INTO resume (active, applicant_id, job_id)
-  SELECT active, map_applicant.new_id, map_job.new_id
-  FROM tmp_resume
-  JOIN map_applicant ON (tmp_resume.applicant_id=map_applicant.map_id)
-  JOIN map_job ON (tmp_resume.job_id = map_job.map_id)
-  ORDER BY resume_id ASC OFFSET 0 LIMIT 1000
-  RETURNING resume_id
-)
-INSERT INTO map_resume (new_id)
-SELECT resume_id FROM ins;
-
+INSERT INTO resume (resume_id, active, applicant_id, job_id)
+SELECT tmp_resume.new_id, active, tmp_applicant.new_id, tmp_job.new_id
+FROM tmp_resume
+JOIN tmp_applicant USING (applicant_id)
+JOIN tmp_job USING (job_id)
+ORDER BY resume_id ASC OFFSET 0 LIMIT 1000;
 
 -- message
-WITH ins AS (
-  INSERT INTO message (send, account_id, vacancy_id, resume_id, text)
-  SELECT send, map_account.new_id, map_vacancy.new_id, map_resume.new_id, text
-  FROM tmp_message
-  JOIN map_account ON (tmp_message.account_id = map_account.map_id)
-  JOIN map_vacancy ON (tmp_message.vacancy_id = map_vacancy.map_id)
-  JOIN map_resume ON (tmp_message.resume_id = map_resume.map_id)
-  ORDER BY message_id ASC OFFSET 0 LIMIT 1000
-  RETURNING message_id
-)
-INSERT INTO map_message (new_id)
-SELECT message_id FROM ins;
+INSERT INTO message (message_id, send, account_id, vacancy_id, resume_id, text)
+SELECT tmp_message.new_id, send, tmp_account.new_id, tmp_vacancy.new_id, tmp_resume.new_id, text
+FROM tmp_message
+JOIN tmp_account USING (account_id)
+JOIN tmp_vacancy USING (vacancy_id)
+JOIN tmp_resume USING  (resume_id)
+ORDER BY message_id ASC OFFSET 0 LIMIT 1000;
 
 -- read_message
-WITH ins AS (
-  INSERT INTO read_message (message_id, account_id)
-  SELECT map_message.new_id, map_account.new_id
-  FROM tmp_read_message
-  JOIN map_account ON (tmp_read_message.account_id = map_account.map_id)
-  JOIN map_message ON (tmp_read_message.message_id = map_message.map_id)
-  ORDER BY read_message_id ASC OFFSET 0 LIMIT 1000
-  RETURNING read_message_id
-)
-INSERT INTO map_read_message (new_id)
-SELECT read_message_id  FROM ins;
+INSERT INTO read_message (read_message_id, message_id, account_id)
+SELECT tmp_read_message.new_id, tmp_message.new_id, tmp_account.new_id
+FROM tmp_read_message
+JOIN tmp_message USING (message_id)
+JOIN tmp_account ON (tmp_read_message.account_id = tmp_account.account_id)
+ORDER BY read_message_id ASC OFFSET 0 LIMIT 1000;
 --
